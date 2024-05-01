15b9abb9a134de6a561f0e2446124032e4b51edc
prabhashkaushik <73583966+prabhashkaushik@users.noreply.github.com>
2024-05-01 23:24:42 +0530

    Add files via upload

---
+++ jenkins/Controllers/DevOpsAssignment3.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DevOpsAssignment3.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DevOpsAssignment3 : Controller
+    {
+            
+            [HttpGet]
+            public IActionResult HelloWorld()
+            {
+                return Ok("Hello, world!");
+            }
+    }
+}
